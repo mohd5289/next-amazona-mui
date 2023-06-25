904ffc5697b19eff588994a07eb41dcb5680a34b
Osuolale <mohd5289@yahoo.com>
2023-06-25 16:15:04 +0100

    Finished Implementing Update Product API

--- next-amazona-mui/src/pages/api/admin/products/[id]/index.js
+++ next-amazona-mui/src/pages/api/admin/products/[id]/index.js
@@ -12,4 +12,30 @@ handler.get(async (req, res) => {
   await db.disconnect();
   res.send(product);
 });
+
+handler.put(async (req, res) => {
+  await db.connect();
+  const product = await Product.findById(req.query.id);
+  if (product) {
+    product.name = req.body.name;
+    product.slug = req.body.slug;
+    product.price = req.body.price;
+    product.category = req.body.category;
+    product.image = req.body.image;
+    // product.featuredImage = req.body.featuredImage;
+    product.isFeatured = req.body.isFeatured;
+    product.brand = req.body.brand;
+    product.countInStock = req.body.countInStock;
+    product.description = req.body.description;
+    await product.save();
+    await db.disconnect();
+    res.send({ message: "Product Updated Sucessfully" });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: "Product Not Found" });
+  }
+
+  //   await db.disconnect();
+});
+
 export default handler;
